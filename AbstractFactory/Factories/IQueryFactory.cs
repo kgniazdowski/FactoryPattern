450105fefd6cb2b170ec599302548fed4fe52a77
kgniazdowski <kgniazdowski@inf.ug.edu.pl>
2017-11-08 16:08:24 +0100

    Metoda wytwórcza

--- AbstractFactory/Factories/IQueryFactory.cs
+++ AbstractFactory/Factories/IQueryFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AbstractFactory.Db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace AbstractFactory.Factories
 {
-    public abstract class QueryFactory
+    interface IQueryFactory
     {
+        Query PrepareQuery();
     }
 }
